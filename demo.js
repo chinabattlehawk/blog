2ee8d7a3e5af37aa2ebcfb99726453a0958bd5f1
诸葛战鹰 <battlehawk@qq.com>
2018-05-06 23:55:46 +0800

    Update demo.js

--- demo.js
+++ demo.js
@@ -8,7 +8,7 @@ fs.mkdirSync('css');
 fs.mkdirSync('js');
 
 fs.writeFileSync('./index.html', '<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>');
-fs.writeFileSync('css/style.css', 'h1{color: red;}');
+fs.writeFileSync('./css/style.css', 'h1{color: red;}');
 fs.writeFileSync('./js/main.js', 'var string = "Hello World"'+'\n'+'alert(string)');
 
 process.exit(0)
